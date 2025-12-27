8ea9efe4ade80dcaf0f8178cca49c74dbc29204b
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-27 10:05:47 +0530

    ad

---
+++ Src.js
@@ -0,0 +1,13 @@
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+let seeMoreButtons = document.querySelectorAll('.seeMore');
+let backButton = document.getElementById('back');
+
+nextButton.onclick = function(){
+    showSlider('next');
+}
+prevButton.onclick = function(){
+    showSlider('prev');
+}
